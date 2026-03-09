d69f154555501962639c24423ec26b93498fc06c
Maha <medaboinamahaa@gmail.com>
2026-03-10 03:05:36 +0530

    Initial commit: interior design landing page

---
+++ script.js
@@ -0,0 +1,16 @@
+const backToTop = document.getElementById("backToTop");
+
+window.onscroll = function () {
+if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+backToTop.style.display = "block";
+} else {
+backToTop.style.display = "none";
+}
+};
+
+backToTop.onclick = function () {
+window.scrollTo({
+top: 0,
+behavior: "smooth"
+});
+};
